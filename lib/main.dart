128042916de46ef3d7bd2fda90d13bd7d7a8acfc
SOURAV KUMAR PANDA <74825918+sourav123608@users.noreply.github.com>
2023-12-22 09:39:33 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chatbot_app/chatscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI ChatBot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
